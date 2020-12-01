f8c7ad545871788fbb4a91b4aaf77b2166f6a5e8
Sarai <saraiescamilla4372@gmail.com>
2020-11-30 19:06:55 -0600

    Corrección de errores de Nav Tecnico

--- app/src/main/java/com/example/vanken/MainTecnicoActivity.java
+++ app/src/main/java/com/example/vanken/MainTecnicoActivity.java
@@ -1,43 +1,44 @@
 package com.example.vanken;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainTecnicoActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainTecnicoActivity extends AppCompatActivity {
 
-    DrawerLayout drawerLayoutTecnico;
-    NavigationView navigationViewTecnico;
-    Toolbar toolbarTecnico;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tecnico);
 
-        drawerLayoutTecnico = findViewById(R.id.drawer_layout_tecnico);
-        navigationViewTecnico = findViewById(R.id.nav_tecnico_Menu);
-        toolbarTecnico = findViewById(R.id.toolbarTecnico);
-        setSupportActionBar(toolbarTecnico);
+        toolbar = findViewById(R.id.toolbarTecnicoMain);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout_tecnico);
+        navigationView = findViewById(R.id.nav_tecnico_Menu);
 
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainTecnicoActivity.this, drawerLayout, toolbar, R.string.nav_open_tec, R.string.nav_close_tec);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
 
-        navigationViewTecnico.bringToFront();
-        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(MainTecnicoActivity.this, drawerLayoutTecnico, R.string.nav_open_tec,R.string.nav_close_tec);
-        drawerLayoutTecnico.addDrawerListener(toogle);
-        toogle.syncState();
-        navigationViewTecnico.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return true;
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container_Main_Tecnico,new ListaPeticionesTecnicoFragment());
+        fragmentTransaction.commit();
     }
 }
